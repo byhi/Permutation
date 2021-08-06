d4ee45e9d5b5a27e04763a290148c227d0d94c3c
Bihari Zoltán <byhisoft@gmail.com>
2021-08-06 13:48:15 +0200

    init server modul

---
+++ permutation-server/src/main/java/hu/byhi/app/permutation/permutationserver/PermutationServerApplication.java
@@ -0,0 +1,13 @@
+package hu.byhi.app.permutation.permutationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermutationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermutationServerApplication.class, args);
+	}
+
+}
---
+++ permutation-server/src/test/java/hu/byhi/app/permutation/permutationserver/PermutationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.byhi.app.permutation.permutationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermutationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
